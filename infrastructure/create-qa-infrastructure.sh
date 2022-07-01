5f4fb8e5f7251793981e2bd147e5965691bb7561
ycetin94 <ycetin94@gmail.com>
2022-07-01 11:15:20 +0000

    added jenkinsfile for creating manual qa environment

--- infrastructure/create-qa-infrastructure.sh
+++ infrastructure/create-qa-infrastructure.sh
@@ -4,4 +4,4 @@ ANS_KEYPAIR="matt-${APP_NAME}-qa.key"
 AWS_REGION="us-east-1"
 cd infrastructure/qa-k8s-terraform
 terraform init
-terraform apply -auto-approve  
+terraform apply -auto-approve -no-color
